3c1bd39c2181f47612ab9bd561fef625f636d639
shahriarRahmanShaon <shaon463@gmail.com>
2021-04-19 17:09:23 +0600

    Initial Commit

---
+++ Can it be done with swiftui??/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Can it be done with swiftui??
+//
+//  Created by sergio shaon on 19/4/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
